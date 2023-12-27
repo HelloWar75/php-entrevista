36256554823668db964352988e426a04954ad37b
Luis Justin <luisjuetin72@gmail.com>
2023-12-27 16:47:04 -0300

    Notificações novas para erro ao add user e sucesso ao add user

--- index.php
+++ index.php
@@ -59,6 +59,22 @@ include_once 'layout/header.php';
         ?>
 
         <?php
+        // Usuario deletado com sucesso!
+        if (!empty($_GET['info']) && $_GET['info'] === '2') {
+            ?>
+            <div class="row">
+                <div class="offset-1 col-10">
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        Usuário adicionado com sucesso!
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+
+        <?php
         // Erro ao deletar usuario!
         if (!empty($_GET['error']) && $_GET['error'] === '1') {
             ?>
@@ -98,6 +114,22 @@ include_once 'layout/header.php';
                 <div class="offset-1 col-10">
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
                         Erro ao atualizar o usuário tente novamente!
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+
+        <?php
+        // Erro ao deletar usuario!
+        if (!empty($_GET['error']) && $_GET['error'] === '3') {
+            ?>
+            <div class="row">
+                <div class="offset-1 col-10">
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        Erro ao criar novo usuário!
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 </div>
