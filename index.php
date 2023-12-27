55343061d221a03022e7b997d35ce5aba6b84a87
Luis Justin <luisjuetin72@gmail.com>
2023-12-27 13:59:18 -0300

    Toaster Added

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $users = $connection->query("SELECT * FROM users");
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
 
 <body>
@@ -91,7 +92,7 @@ $users = $connection->query("SELECT * FROM users");
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js" crossorigin="anonymous"></script>
     <script language="JavaScript" type="text/javascript">
         function checkDelete() {
             return confirm('Certeza que deseja deletar o usuário permanentemente ?');
