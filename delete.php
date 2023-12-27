87f7927eff5ea0194918f2c5279a17da37812f21
Luis Justin <luisjuetin72@gmail.com>
2023-12-27 14:00:21 -0300

    Delete script created

---
+++ delete.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'connection.php';
+$connection = new Connection();
+
+// Verificar se $_GET está vazio caso esteja redirecionar e exibir erro
+if (empty($_GET['id'])) {
+    header('Location: index.php?error=1');
+}
+
+$id = $_GET['id'];
+
+// Verifica se usuario existe no banco de dados
+$users = $connection->query("SELECT * FROM users WHERE id = '$id'");
+
+if ($users->fetchColumn() > 0) {
+    $connection->exec("DELETE FROM users WHERE id = '$id'");
+    header('Location: index.php?action=1');
+} else {
+    header('Location: index.php?error=2');
+}
+
+
