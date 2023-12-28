9bfdb58521a35cefa2851d55f29fb5147cf5fdb8
Luis Justin <luisjuetin72@gmail.com>
2023-12-28 14:16:06 -0300

    Tratamento de exceções new_p.php

--- new_p.php
+++ new_p.php
@@ -6,21 +6,29 @@ $userDao = new UserDao($db);
 $colorDao = new ColorDao($db);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $colors_ids = $_POST['colors'];
 
-    $user = new User($name, $email);
-    
-    $colors = [];
+    try {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $colors_ids = $_POST['colors'];
 
-    foreach($colors_ids as $color_id) {
-        array_push($colors, $colorDao->getColorById($color_id));
+        $user = new User($name, $email);
+
+        $colors = [];
+
+        foreach ($colors_ids as $color_id) {
+            array_push($colors, $colorDao->getColorById($color_id));
+        }
+
+        $user->setColors($colors);
+
+        $userDao->insert($user);
+
+        notifyAndRedir('success', 'Usuário adicionadop com sucesso!', 'index.php');
+
+    } catch (Exception $e) {
+        notifyAndRedir('danger', $e->getMessage(), 'index.php');
     }
 
-    $user->setColors($colors);
 
-    $userDao->insert($user);
-
-    notifyAndRedir('success', 'Usuário adicionado com sucesso!', 'index.php');
 }
